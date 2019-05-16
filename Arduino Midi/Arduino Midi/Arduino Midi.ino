cb9f2e22d4b26e001e5cb05356ab917d7f7bf190
Kevin Jiang <kevin51jiang@email.com>
2019-05-16 16:49:04 -0400

    initial commit

---
+++ Arduino Midi/Arduino Midi/Arduino Midi.ino
@@ -0,0 +1,24 @@
+﻿/*
+ Name:		Arduino_Midi.ino
+ Created:	5/16/2019 1:00:41 🍺
+ Author:	Kevin J
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <MIDI.h>
+
+
+MIDI_CREATE_CUSTOM_INSTANCE();
+
+void setup() {
+	MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	// Send note 42 with velocity 127 on channel 1
+	MIDI.sendNoteOn(42, 127, 1);
+	delay(1000);
+	MIDI.read();
+}
